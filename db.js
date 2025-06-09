03411fd9bb3653215a31e919aa27424fec7a3150
Jeff himself <162985697+jeff0proprio@users.noreply.github.com>
2025-06-09 11:55:22 -0300

    Add files via upload

---
+++ db.js
@@ -0,0 +1,77 @@
+// const { Pool } = require("pg");
+
+async function connect() {  
+  const { Pool } = require("pg");
+
+  if(global.connection)
+      return global.connection.connect();
+
+  const pool = new Pool({
+    user: process.env.USER_NAME,
+    host: process.env.HOST_NAME,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.PORT_NUMBER
+  });
+  
+  const client = await pool.connect();
+  console.log("O Pool de conexão foi criado com sucesso!")
+  client.release();
+
+  global.connection = pool;
+  
+  return pool.connect();
+}
+
+connect();
+
+// Função para listar clientes
+async function selectCustomers() {
+// Estabelecer conexão com o banco de dados
+const client = await connect();
+// Enviar comando SQL para o banco de dados
+const res = await client.query("SELECT * FROM client");
+// Retorna as linhas (registros) da tabela
+return res.rows;
+}
+
+// Função para listar um cliente
+async function selectCustomer(id) {
+// Estabelece a conexão com o banco de dados
+const client = await connect();
+// Executa a query SQL usando declaração preparada para evitar SQL Injection
+const res = await client.query("SELECT * FROM client WHERE cpf=$1", [id]);
+// Retorna as linhas (dados do cliente)
+return res.rows;
+}
+
+// Função para inserir clientes
+async function insertCustomers(customer) {
+// Estabelecer conexão
+const client = await connect();
+// Query
+const sql = "INSERT INTO client(cpf, nome, email, idade, profissao) VALUES($1, $2, $3, $4, $5);";
+// Parâmetros injetados na função
+const values = [customer.cpf, customer.nome, customer.email, customer.idade, customer.profissao];
+
+await client.query(sql, values);
+}
+
+// Função para excluir cliente
+async function deleteCustomer(id) {
+// Estabelecer conexão
+const client = await connect();
+// parâmetros que devem ser injetados
+const sql = "DELETE FROM client WHERE cpf=$1";
+const values = [id];
+
+await client.query(sql, values)
+}
+
+module.exports = {
+insertCustomers,
+selectCustomers,
+selectCustomer,
+deleteCustomer
+}
---
+++ index.js
@@ -0,0 +1,44 @@
+require("dotenv").config(); 
+
+const db = require("./db");
+
+const port = process.env.PORT;
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+// Rota para listar todos os clientes
+app.get('/client', async (req, res) => {
+    // Chama a função que seleciona os clientes no banco de dados
+    const clientes = await db.selectCustomers();
+    // Envia a resposta em formato JSON contendo os clientes
+    res.json(clientes);
+    });
+
+// Rota para listar um cliente específico
+app.get('/client/:id', async (req, res) => {
+    // Captura o parâmetro 'id' presente na URL, que corresponde ao CPF do cliente
+    const cliente = await db.selectCustomer(req.params.id);
+    // Responde com os dados do cliente em formato JSON
+    res.json(cliente);
+    });
+
+// Rota para inserir clientes
+app.post("/client", async (req, res) => {
+    await db.insertCustomers(req.body),
+
+    res.sendStatus(201);
+});
+
+// Rota para excluir cliente
+app.delete("/client/:id", async (req, res) => {
+    await db.deleteCustomer(req.params.id)
+    res.sendStatus(204)
+});
+
+app.listen(port);
+
+console.log("Backend Rodando!");
